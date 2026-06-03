b5cf8e6c3eb73025aad85f5d41e6d7d8978de3bb
Muchembe Kihumba <muchembekihumba@gmail.com>
2026-06-03 18:57:49 +0300

    stock

--- database.py
+++ database.py
@@ -35,49 +35,8 @@ def get_stock():
     cur.execute('select * from stock')
     return cur.fetchall()
 
-def sales_per_day():
-    cur.execute("""
-        SELECT DATE(created_at), SUM(quantity)
-        FROM sales
-        GROUP BY DATE(created_at)
-        ORDER BY DATE(created_at)
-    """)
-    return cur.fetchall()
 
-def profit_per_day():
-    cur.execute("""
-        SELECT DATE(s.created_at), SUM((p.selling_price - p.buying_price) * s.quantity)
-        FROM sales s
-        JOIN products p ON s.pid = p.id
-        GROUP BY DATE(s.created_at)
-        ORDER BY DATE(s.created_at)
-    """)
-    return cur.fetchall()
-
-def sales_per_product():
-    cur.execute("""
-        SELECT p.name, SUM(s.quantity)
-        FROM sales s
-        JOIN products p ON s.pid = p.id
-        GROUP BY p.name
-        ORDER BY SUM(s.quantity) DESC
-    """)
-    return cur.fetchall()
-
-def profit_per_product():
-    cur.execute("""
-        SELECT p.name, SUM((p.selling_price - p.buying_price) * s.quantity) AS profit
-        FROM sales s
-        JOIN products p ON s.pid = p.id
-        GROUP BY p.name
-        ORDER BY profit DESC
-    """)
-    return cur.fetchall()
 
 print(get_products())
 print(get_sales())
 print(get_stock())
-print(sales_per_day())
-print(profit_per_day())
-print(sales_per_product())
-print(profit_per_product())
